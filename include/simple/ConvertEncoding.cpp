f7fce257fde026ed65fbd047383b1cfe7c663247
sskorokhodov <skorohodovsemen@gmail.com>
2013-05-11 00:50:34 +0400

    Winows dependency deleted, but non-ascii text would be unavailable in windows.

--- include/simple/ConvertEncoding.cpp
+++ include/simple/ConvertEncoding.cpp
@@ -3,7 +3,7 @@
 #include <simple/ConvertEndoding.h>
 #include <vector>
 
-#include <Windows.h>
+//#include <Windows.h>
 
 
 
@@ -14,15 +14,19 @@ namespace simple
 std::string
 convertAnsiToOem( const std::string& ansi )
 {
+	/*
 	std::string oem( ansi.size(), '\0' );
 	::AnsiToOem( ansi.c_str(), const_cast< char* >( oem.c_str() ) );
 	return oem;
+	*/
+	return ansi;
 }
 
 
 std::string
 convertUtf8ToOem( const std::string& utf8 )
 {
+	/*
 	int nBytes = ::MultiByteToWideChar( CP_UTF8, 0, utf8.c_str(), -1, 0, 0 );
 	std::vector< wchar_t > wbuffer( nBytes );
 	::MultiByteToWideChar( CP_UTF8, 0, utf8.c_str(), -1, &wbuffer.front(), nBytes );
@@ -35,6 +39,8 @@ convertUtf8ToOem( const std::string& utf8 )
 	oem.pop_back();
 
 	return oem;
+	*/
+	return utf8;
 }
 
 
