08db63ee1ed601c0a35c2dde917cb6b760fcc5f4
sskorokhodov <skorohodovsemen@gmail.com>
2013-05-11 16:50:03 +0400

    BaseNode class doxygen comment.

--- include/rstyle/nodestree/BaseNode.hpp
+++ include/rstyle/nodestree/BaseNode.hpp
@@ -11,6 +11,15 @@ namespace rstyle
 {
 
 
+/**
+ * @brief The BaseNode class is intend to supply *minimal non-template node interface*.
+ *
+ * Usually to work with that class it is the best choice when you need only to add nodes to tree or
+ * just need for simple node attributes like name or string value.
+ *
+ * When you need to walk through nodes tree, or keep/process nodes data, you have to look for more
+ * complicated derived class.
+ */
 class BaseNode
 {
 public :
